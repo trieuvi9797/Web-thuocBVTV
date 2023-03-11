9ed41797ec271adc02cebe0d932d54fbaaed25db
trieuvi9797 <85662812+trieuvi9797@users.noreply.github.com>
2023-03-11 22:20:56 +0700

    setup database

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title></title>
+        
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+
+    <!-- FontAwesome JS-->
+    <script defer src="{{ asset('admin/assets/plugins/fontawesome/js/all.min.js') }}"></script>
+
+    <!-- App CSS -->  
+    <link id="theme-style" rel="stylesheet" href="{{ asset('admin/assets/css/portal.css') }}">
+
+    <!-- App CKeditor -->  
+    {{-- <script src="../ckeditor.js"></script> --}}
+</head>
+<body class="app">   	
+
+@include('admin.layouts.header')
+
+@include('admin.layouts.sidebar')
+
+@yield('content')
+
+@include('admin.layouts.footer')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard.index');
+});
+Route::get('/home', function () {
+    return view('client.home.index');
+});
+
+Auth::routes();
+
